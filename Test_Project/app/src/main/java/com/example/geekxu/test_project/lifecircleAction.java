f10f0c2a784a4180dc5977e16f7b1404091b2bd6
whucsxu <whucsxu@163.com>
2016-10-09 16:58:07 +0800

    导入

---
+++ Test_Project/app/src/main/java/com/example/geekxu/test_project/lifecircleAction.java
@@ -0,0 +1,13 @@
+package com.example.geekxu.test_project;
+
+import android.os.Bundle;
+import android.app.Activity;
+/**
+ * Created by Geek Xu on 2016/9/23.
+ */
+public class lifecircleAction extends  Activity{
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lifecircle);
+}
+}
---
+++ Test_Project/app/src/main/java/com/example/geekxu/test_project/secondActivity.java
@@ -0,0 +1,14 @@
+package com.example.geekxu.test_project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Geek Xu on 2016/9/23.
+ */
+public class secondActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.two);
+    }
+}
